5af04c4f81bb3ae5dedcdd8662a7bfe2dcc2e48d
Aysun Taghizade <aysuntaghizade@gmail.com>
2024-02-21 22:26:31 +0400

    first commit

---
+++ validator.js
@@ -0,0 +1,16 @@
+//!emailValidator
+// const emailValidator=/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+// console.log(emailValidator.test("aysuntaghizade@gmail.com"));
+
+//!phone number
+// const phoneV=/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
+// console.log(phoneV.test("+994772731355 "));
+
+//!...
+//  class Regexs {
+//  static email=/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+//  static number=/'^\d+$'/;
+//  static password=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+//  static age=/^\S[0-9]{0,3}$/;
+// }
+// console.log(Regexs);
