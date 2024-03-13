fc57921f9db705defab0287c8ef1bfe2d503fa4f
Aysun Taghizade <aysuntaghizade@gmail.com>
2024-03-13 16:21:35 +0400

    new commit

---
+++ apirequest.js
@@ -0,0 +1,42 @@
+//!1
+// function getData(id){
+//     fetch(id)
+//     .then((response)=> response.json())
+//     .then((data)=>console.log(data))
+//     .catch((err)=>console.log(err))
+// }
+// getData("https://api.escuelajs.co/api/v1/users")
+
+//!2
+// const callApi= ()=>{
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(response => response.json())
+//     .then(json => console.log(json))
+
+// }
+// callApi()
+
+//!3
+// const  Api= ()=>{
+//     fetch('https://fakestoreapi.com/products/1')
+//     .then(res=>res.json())
+//     .then(json=>console.log(json))
+// }
+// Api()
+
+//!4
+// function sayt(api){
+//     fetch(api)
+//     .then((response)=> response.json())
+//     .then((data)=>console.log(data))
+//     .catch((err)=>console.log(err))
+// }
+// sayt('https://api.escuelajs.co/api/v1/users/id/1')
+
+//!5
+// const api=()=>{
+//     fetch('https://dummyjson.com/products/1')
+// .then(res => res.json())
+// .then(json => console.log(json))
+// }
+// api()
